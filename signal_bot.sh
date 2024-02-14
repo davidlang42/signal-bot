f603551245443c6e801461cbf1eebf9d051b53b7
David Lang <davidlang42@gmail.com>
2024-02-15 02:26:53 +1100

    Add verbose output for debug

--- signal_bot.sh
+++ signal_bot.sh
@@ -158,7 +158,7 @@ handle_reaction() { # pid msg_author msg_receiver msg_timestamp
 }
 
 remove_emoji() { # msg_author msg_receiver msg_timestamp
-    signal-cli --config "$CONFIG" sendReaction -r -e "$TASK_EMOJI" -t "$3" -a "$1" "$2"
+    signal-cli -v --config "$CONFIG" sendReaction -r -e "$TASK_EMOJI" -t "$3" -a "$1" "$2"
 }
 
 # main loop
