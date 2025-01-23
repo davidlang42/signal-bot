5b1493815f952538371d56c5f714f916ce063333
David Lang <davidlang42@gmail.com>
2025-01-24 00:01:29 +1100

    Restructure code

--- signal_bot.py
+++ signal_bot.py
@@ -11,11 +11,16 @@ import qrcode # pip install qrcode
 TASK_EMOJI="☑️"
 MESSAGES="/signal_bot_messages"
 CONFIG="/signal_bot_config"
+NAME_SEPARATOR=": "
+MAX_TITLE_LENGTH=60
+TITLE_TRUNCATION="..."
 
 appsScriptUrl = os.getenv("GOOGLE_APPS_SCRIPT_URL")
 if not appsScriptUrl:
      print("Must set environment variable GOOGLE_APPS_SCRIPT_URL")
      exit(1)
+
+### Google Apps Scripts API
 
 def SendEmail(subject, html):
     response = requests.get(appsScriptUrl, {
@@ -43,6 +48,8 @@ def AddTask(title, notes):
     else:
         print(f"Failed ({response.status_code}) to add task '{title}': {response.text}")
         return False
+    
+### Interact with signal-cli
 
 def LinkDevice():
     process = subprocess.Popen(['signal-cli', '--config', CONFIG, 'link', '-n', "SignalBot"], stdout=subprocess.PIPE)
@@ -62,36 +69,40 @@ def LinkDevice():
 def ListenForMessages():
     return subprocess.Popen(['signal-cli', '--config', CONFIG, '-o', 'json', 'receive', '-t', '-1', '--ignore-attachments', '--ignore-stories', '--max-messages', '10'], stdout=subprocess.PIPE, universal_newlines=True)
 
-#TODO TEST BELOW HERE
+def RemoveEmoji(author, receiver, timestamp):
+    listener.terminate() # otherwise executing signal-cli below will fail
+    subprocess.run(['signal-cli', '--config', CONFIG, 'sendReaction', '-r', '-e', TASK_EMOJI, '-t', timestamp, '-a', author, receiver])
+
+### Parse JSON payloads
 
 def ProcessPayload(p):
-    print(f"PAYLOAD: {p}")
     if "envelope" in p:
         ProcessEnvelope(p["envelope"], p["account"])
 
+#TODO TEST BELOW HERE
+
 def ProcessEnvelope(e, account):
     print(f"ENVELOPE: {e}")
-    source = e["source"]
     if "syncMessage" in e and "sentMessage" in e["syncMessage"]:
         # I sent a message or I reacted
         sent = e["syncMessage"]["sentMessage"]
+        source = e["source"]
         dest = sent["destination"]
-        CheckMessageForContent(sent, source, dest, f"to {dest}")
+        CheckMessageForContent(sent, source, dest, e["sourceName"])
         CheckMessageForReaction(sent, source, dest)
-    if "dataMessage" in e:
+    elif "dataMessage" in e:
         # they sent a messsage or they reacted (but ignore their reactions)
-        received = e["dataMessage"]
-        source_name = e["sourceName"]
-        CheckMessageForContent(received, source, account, f"from {source_name}")
+        CheckMessageForContent(e["dataMessage"], e["source"], account, e["sourceName"])
 
-def CheckMessageForContent(m, source, dest, from_to_display_name):
+def CheckMessageForContent(m, source, dest, name):#TODO rename and revise args
     print(f"CHECK-MSG-CONTENT: {m}")
     if not "message" in m:
         return
     message = m["message"]
     timestamp = m["timestamp"]
-    StoreMessage(message, source, dest, timestamp, from_to_display_name)
+    StoreMessage(source, dest, timestamp, name, message)
     if "quote" in m:
+        #TODO revise this branch (needs testing)
         reply = m["quote"]
         reply_to_author = reply["author"]
         reply_to_timestamp = reply["id"]
@@ -99,37 +110,46 @@ def CheckMessageForContent(m, source, dest, from_to_display_name):
         if reply_to_author == reply_to_receiver:
             reply_to_receiver = source
         previous_message = ReadMessage(reply_to_author, reply_to_receiver, reply_to_timestamp)
-        AppendMessage(previous_message, source, dest, timestamp)
+        AppendMessage(source, dest, timestamp, previous_message)
 
-def CheckMessageForReaction(m, source, dest):
+def CheckMessageForReaction(m, source, dest):#TODO rename and revise args
     print(f"CHECK-MSG-REACTION: {m}")
     if not "reaction" in m:
         return
-    emoji = m["reaction"]["emoji"]
-    remove = m["reaction"]["isRemove"]
+    reaction = m["reaction"]
+    emoji = reaction["emoji"]
+    remove = reaction["isRemove"]
     if emoji == TASK_EMOJI and not remove:
-        author = m["reaction"]["targetAuthor"]
-        timestamp = m["reaction"]["targetSentTimestamp"]
+        author = reaction["targetAuthor"]
+        timestamp = reaction["targetSentTimestamp"]
         if author == dest:
             HandleReaction(author, source, timestamp)
         else:
             HandleReaction(author, dest, timestamp)
 
-def StoreMessage(message, author, receiver, timestamp, from_to_display_name):
-    print(f'TODO---STORE: {message}')
+### Persist messages by (author, receiver, timestamp)
+
+def StoreMessage(author, receiver, timestamp, name, message):
+    print(f'[{author}, {receiver}, {timestamp}] {name}{NAME_SEPARATOR}{message}') #TODO remove
+    heading = (message[:MAX_TITLE_LENGTH-len(TITLE_TRUNCATION)] + TITLE_TRUNCATION) if len(message) > MAX_TITLE_LENGTH else message
+    body = name + NAME_SEPARATOR + message
     #TODO: echo "$1" > "$(message_path "$2" "$3" "$4")"
     #TODO: echo "(Signal message $5)" >> "$(message_path "$2" "$3" "$4")"
 
-def AppendMessage(message, author, receiver, timestamp):
-    print(f'TODO---APPEND: {message}')
+def AppendMessage(author, receiver, timestamp, previous_message):
+    print(f'TODO---APPEND: {previous_message}')
+    #TODO: skip the first line of previous_message, because thats just the (truncated) heading
     #TODO: echo "$1" >> "$(message_path "$2" "$3" "$4")"
 
 def ReadMessage(author, receiver, timestamp):
     print(f'TODO---READ')
+    #TODO: read the whole thing, including the (truncated) heading on the first line
     #TODO: cat "$(message_path "$1" "$2" "$3")"
 
 def MessagePath(author, receiver, timestamp):
     return f"{MESSAGES}/{author}_{receiver}_{timestamp}"
+
+### Actually be a Signal bot
 
 def HandleReaction(author, receiver, timestamp):
     lines = ReadMessage(author, receiver, timestamp)
@@ -142,20 +162,10 @@ def HandleReaction(author, receiver, timestamp):
     if AddTask(name, notes):
         RemoveEmoji(author, receiver, timestamp)
 
-def RemoveEmoji(author, receiver, timestamp):
-    listener.terminate() # otherwise executing signal-cli below will fail
-    subprocess.run(['signal-cli', '--config', CONFIG, 'sendReaction', '-r', '-e', TASK_EMOJI, '-t', timestamp, '-a', author, receiver])
+### Main loop
 
-
-
-
-
-    
-
-
-# main loop
 while True:
-    listener = ListenForMessages()
+    listener = ListenForMessages() # global so RemoveEmoji can terminate it
     for line in iter(listener.stdout.readline, ""):
         ProcessPayload(json.loads(line))
     if listener.wait() != 0: # non-zero return code indicates device is not linked
