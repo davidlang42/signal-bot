6229968b095526000dec66297d50756ab6da8dd4
David Lang <davidlang42@gmail.com>
2025-01-23 18:26:02 +1100

    Translate bash to python

--- signal_bot.py
+++ signal_bot.py
@@ -1,178 +1,154 @@
-#!/usr/bin/python3
-import sys
-print(sys.version)
-exit()
+#!/usr/bin/python
+import os
+import io
+import base64
+import subprocess
+import json
+import time
+import requests # pip install requests
+import qrcode # pip install qrcode
 
+TASK_EMOJI="☑️"
+MESSAGES="/signal_bot_messages"
+CONFIG="/signal_bot_config"
 
-# set -eu
+appsScriptUrl = os.getenv("GOOGLE_APPS_SCRIPT_URL")
+if not appsScriptUrl:
+     print("Must set environment variable GOOGLE_APPS_SCRIPT_URL")
+     exit(1)
 
-# if [ -z "${GOOGLE_APPS_SCRIPT_URL}" ]; then
-#     echo "Must set environment variable GOOGLE_APPS_SCRIPT_URL"
-#     exit 1 # failed
-# fi
+def SendEmail(subject, html):
+    response = requests.get(appsScriptUrl, {
+        'action':'email',
+        'title': subject,
+        'html': html
+    })
+    if response.status_code == 200:
+        print(f"Sent email '{subject}'")
+        return True
+    else:
+        print(f"Failed ({response.status_code}) to send email '{subject}': {response.text}")
+        return False
 
-# set -x
+def AddTask(title, notes):
+    #TODO: local due=$(date -d "$DATE_ADJUSTMENT" +%Y-%m-%d)
+    response = requests.get(appsScriptUrl, {
+        'action':'task',
+        'due': due,
+        'title': title,
+        'notes': notes
+    })
+    if response.status_code == 200:
+        print(f"Added task '{title}'")
+        return True
+    else:
+        print(f"Failed ({response.status_code}) to add task '{title}': {response.text}")
+        return False
 
-# TASK_EMOJI="☑️"
-# MESSAGES="/signal_bot_messages"
-# CONFIG="/signal_bot_config"
+def LinkDevice():
+    process = subprocess.Popen(['signal-cli', '--config', CONFIG, 'link', '-n', "SignalBot"], stdout=subprocess.PIPE)
+    link = process.stdout.readline().decode()
+    qr = qrcode.make(link)
+    memory_buffer = io.BytesIO()
+    qr.save(memory_buffer, format="PNG")
+    base64string = base64.b64encode(memory_buffer.getvalue()).decode()
+    html = f"<p>{link}</p><img src='data:image/png;base64,{base64string}' />"
+    if not SendEmail("SignalBot is not linked", html):
+        print("Failed to send link email")
+        process.terminate()
+        return False
+    returncode = process.wait()
+    return returncode == 0
 
-# set -o pipefail # link_device, read_messages
+def WaitForMessage(): #TODO: formerly read_messages
+    #TODO: could try
+    # popen = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True)
+    # for stdout_line in iter(popen.stdout.readline, ""):
+    #     yield stdout_line 
+    # popen.stdout.close()
+    # return_code = popen.wait()
+    #TODO: previously tried to process messages continuously until actually nessesary to terminate it, now just reads 1 at a time (will need to test if this is performant)
+    result = subprocess.run(['signal-cli', '--config', CONFIG, '-o', 'json', 'receive', '-t', '-1', '--ignore-attachments', '--ignore-stories', '--max-messages', '1'], stdout=subprocess.PIPE)
+    if result.returncode != 0:
+        return False
+    msg = json.loads(result.stdout.decode())
+    ProcessMessage(msg)
+    return True
 
-# send_email() { # subject html
-#     curl -G "$GOOGLE_APPS_SCRIPT_URL" --data-urlencode "action=email" --data-urlencode "title=$1" --data-urlencode "html=$2"
-# }
+def ProcessMessage(m):
+    source = m["envelope"]["source"]
+    sent = m["envelope"]["syncMessage"]["sentMessage"]
+    if sent:
+        # I sent a message or I reacted
+        dest = sent["destination"]
+        #TODO: check_for_content "$sent" "$source" "$destination" "to $destination"
+        #TODO: check_for_reaction "$pid" "$sent" "$source" "$destination"
+    received = m["envelope"]["dataMessage"]
+    if received:
+        # they sent a messsage or they reacted (but ignore their reactions)
+        my_account = m["account"]
+        source_name = m["envelope"]["sourceName"]
+        #TODO: check_for_content "$received" "$source" "$my_account" "from $source_name"
 
-# add_task() { # title notes
-#     local due=$(date -d "$DATE_ADJUSTMENT" +%Y-%m-%d)
-#     curl -G "$GOOGLE_APPS_SCRIPT_URL" --data-urlencode "action=task" --data-urlencode "due=$due" --data-urlencode "title=$1" --data-urlencode "notes=$2"
-# }
+def CheckForContent(o, source, dest, from_to_display_name):
+    message = o["message"]
+    if not message:
+        return False
+    timestamp = o["timestamp"]
+    StoreMessage(message, source, dest, timestamp, from_to_display_name)
+    reply = o["quote"]
+    if reply:
+        reply_to_author = reply["author"]
+        reply_to_timestamp = reply["id"]
+        reply_to_receiver = dest
+        if reply_to_author == reply_to_receiver:
+            reply_to_receiver = source
+        previous_message = ReadMessage(reply_to_author, reply_to_receiver, reply_to_timestamp)
+        AppendMessage(previous_message, source, dest, timestamp)
 
-# send_link_email() {
-#     local link
-#     read -r link
-#     local png=$(qrencode -o - "$link" | base64 -w 0)
-#     local html="<p>$link</p><img src=\"data:image/png;base64,$png\" />"
-#     if ! send_email "SignalBot is not linked" "$html"
-#     then
-#         echo Failed to send email
-#         exit 1
-#     fi
-#     local rest=$(cat)
-# }
+def CheckForReaction(o, source, dest):
+    emoji = o["reaction"]["emoji"]
+    remove = o["reaction"]["isRemove"].strip().lower() == "true"
+    if emoji == TASK_EMOJI and not remove:
+        author = o["reaction"]["targetAuthor"]
+        timestamp = o["reaction"]["targetSentTimestamp"]
+        if author == dest:
+            HandleReaction(author, source, timestamp)
+        else:
+            HandleReaction(author, dest, timestamp)
 
-# link_device() {
-#     # set -o pipefail
-#     signal-cli --config "$CONFIG" link -n "SignalBot" | send_link_email
-# }
+def StoreMessage(message, author, receiver, timestamp, from_to_display_name):
+    print(message)
+    #TODO: echo "$1" > "$(message_path "$2" "$3" "$4")"
+    #TODO: echo "(Signal message $5)" >> "$(message_path "$2" "$3" "$4")"
 
-# read_messages() {
-#     # set -o pipefail
-#     sh -c "echo \$\$; exec signal-cli --config \"$CONFIG\" -o json receive -t -1 --ignore-attachments --ignore-stories" | process_messages
-#     result=$?
-#     if [[ $result -eq 143 ]]
-#     then
-#         # correctly terminated by process_messages
-#         return 0
-#     else
-#         return $result
-#     fi
-# }
+def AppendMessage(message, author, receiver, timestamp):
+    print(message)
+    #TODO: echo "$1" >> "$(message_path "$2" "$3" "$4")"
 
-# process_messages() {
-#     local pid
-#     read -r pid
-#     local line
-#     while read -r line
-#     do
-#         local source="$(jq -r .envelope.source <<< "$line")"
-#         local sent
-#         if sent="$(jq -e .envelope.syncMessage.sentMessage <<< "$line")"
-#         then
-#             # I sent a message or I reacted
-#             local destination="$(jq -r .destination <<< "$sent")"
-#             check_for_content "$sent" "$source" "$destination" "to $destination"
-#             check_for_reaction "$pid" "$sent" "$source" "$destination"
-#         fi
-#         local received
-#         if received="$(jq -e .envelope.dataMessage <<< "$line")"
-#         then
-#             # they sent a messsage or they reacted (but ignore their reactions)
-#             local my_account="$(jq -r .account <<< "$line")"
-#             local source_name="$(jq -r .envelope.sourceName <<< "$line")"
-#             check_for_content "$received" "$source" "$my_account" "from $source_name"
-#         fi
-#     done
-# }
+def ReadMessage(author, receiver, timestamp):
+    print("read message")
+    #TODO: cat "$(message_path "$1" "$2" "$3")"
 
-# check_for_content() { # msg_json source destination from_to_display_name
-#     local message
-#     if message="$(jq -e -r .message <<< "$1")"
-#     then
-#         local timestamp="$(jq -r .timestamp <<< "$1")"
-#         store_message "$message" "$2" "$3" "$timestamp" "$4"
-#         local reply
-#         if reply="$(jq -e .quote <<< "$1")"
-#         then
-#             local reply_to_author="$(jq -r .author <<< "$reply")"
-#             local reply_to_timestamp="$(jq -r .id <<< "$reply")"
-#             local reply_to_receiver="$3"
-#             if [ "$reply_to_author" = "$reply_to_receiver" ]
-#             then
-#                 reply_to_receiver="$2"
-#             fi
-#             local previous_message="$(read_message "$reply_to_author" "$reply_to_receiver" "$reply_to_timestamp")"
-#             append_message "$previous_message" "$2" "$3" "$timestamp"
-#         fi
-#     fi
-# }
+def MessagePath(author, receiver, timestamp):
+    return f"{MESSAGES}/{author}_{receiver}_{timestamp}"
 
-# check_for_reaction() { # pid msg_json source destination
-#     local emoji="$(jq -r .reaction.emoji <<< "$2")"
-#     local remove="$(jq -r .reaction.isRemove <<< "$2")"
-#     if [ "$emoji" = "$TASK_EMOJI" ] && [ "$remove" = "false" ]
-#     then
-#         local author="$(jq -r .reaction.targetAuthor <<< "$2")"
-#         local timestamp="$(jq -r .reaction.targetSentTimestamp <<< "$2")"
-#         if [ "$author" = "$4" ]
-#         then
-#             handle_reaction "$1" "$author" "$3" "$timestamp"
-#         else
-#             handle_reaction "$1" "$author" "$4" "$timestamp"
-#         fi
-#     fi
-# }
+def HandleReaction(author, receiver, timestamp):
+    lines = ReadMessage(author, receiver, timestamp)
+    if len(lines) < 1:
+        name = "Unknown Task"
+        notes = f"Signal message from {author} to {receiver} at {timestamp} could not be found"
+    else:
+        name = lines[0]
+        notes = lines[1:]
+    if AddTask(name, notes):
+        RemoveEmoji(author, receiver, timestamp)
 
-# store_message() { # message msg_author msg_receiver msg_timestamp from_to_display_name
-#     echo "$1" > "$(message_path "$2" "$3" "$4")"
-#     echo "(Signal message $5)" >> "$(message_path "$2" "$3" "$4")"
-# }
+def RemoveEmoji(author, receiver, timestamp):
+    subprocess.run(['signal-cli', '--config', CONFIG, 'sendReaction', '-r', '-e', TASK_EMOJI, '-t', timestamp, '-a', author, receiver])
 
-# append_message() { # message msg_author msg_receiver msg_timestamp
-#     echo "$1" >> "$(message_path "$2" "$3" "$4")"
-# }
-
-# read_message() { # msg_author msg_receiver msg_timestamp
-#     cat "$(message_path "$1" "$2" "$3")"
-# }
-
-# message_path() { # msg_author msg_receiver msg_timestamp
-#     echo "$MESSAGES/${1}_${2}_${3}"
-# }
-
-# handle_reaction() { # pid msg_author msg_receiver msg_timestamp
-#     kill "$1" # so that we can use remove_emoji below
-#     local name="$(read_message "$2" "$3" "$4" | head -n1)"
-#     local notes="$(read_message "$2" "$3" "$4" | sed 1d)"
-#     if [[ -z "$name" ]]
-#     then
-#         name="Unknown Task"
-#         notes="Signal message from $2 to $3 at $4 could not be found"
-#     fi
-#     if ! add_task "$name" "$notes"
-#     then
-#         echo Failed to add task: "$name"
-#         exit 1
-#     fi
-#     if ! remove_emoji "$2" "$3" "$4"
-#     then
-#         echo Failed to remove emoji: "$2" "$3" "$4"
-#         exit 1
-#     fi
-# }
-
-# remove_emoji() { # msg_author msg_receiver msg_timestamp
-#     signal-cli --config "$CONFIG" sendReaction -r -e "$TASK_EMOJI" -t "$3" -a "$1" "$2"
-# }
-
-# # main loop
-# while true
-# do
-#     if ! read_messages # blocks until an emoji gets processed
-#     then
-#         if ! link_device # tries for 1min
-#         then
-#             sleep 240 # wait 1+4min between repeated link emails
-#         fi
-#     fi
-# done
+# main loop
+while True:
+    if not WaitForMessage(): # blocks until a message arrives
+        if not LinkDevice(): # tries for 1min
+            time.sleep(240) # wait 1+4min between repeated link emails
